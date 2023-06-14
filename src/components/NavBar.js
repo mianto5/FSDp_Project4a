ab5a6ef563a95bab5da14f76b9f8fc92fc5d194f
mianto5 <tomanovamichaela5@gmail.com>
2023-06-14 16:55:49 +0200

    Corrects navigation bar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ export default function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
             <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" href="/">
+              <NavLink className="nav-link" aria-current="page" to="/">
                 Home
               </NavLink>
             </li>
